014fc7e1fc67311e97e63ddc85724cdf5c2db1a9
Aye Daniel A <aadaniel108@gmail.com>
2021-01-25 11:56:16 +0100

    drop email index and column

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-    before_save { username.downcase! }
-    validates_uniqueness_of :name
-    validates :username, presence: true, length: { maximum: 50 }, uniqueness: true
-    validates :name, presence: true, length: { minimum: 3, maximum: 25 }
-    has_many :groups
-    has_many :transactions
-end
